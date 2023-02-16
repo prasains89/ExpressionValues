3d3f1714987ae5f1d8a5e96a37cfea8794dce515
Sabina Prasain <prasainsabina@gmail.com>
2023-02-16 13:02:16 -0600

    commit message

---
+++ src/ExpressionValues.java
@@ -0,0 +1,32 @@
+public class ExpressionValues {
+    public static void main(String[] args) {
+        int a = 4 + 6 * 2;
+        int b = 10 / 5 + 8;
+        int c = 12 / 4 + 16 / 2;
+        int d = 17 / 2;
+        int e = 39 / 10;
+        int f = 19 % (2 + 3);
+        int g = 3 + 4 * 20 / 3;
+        int h = 36 % (6 + 2);
+        int i = 8 % 2 * 0;
+        int j = 3 - 5 % 7;
+        int k = 20 / (4 + 1);
+        double l = 18.0 / 4;
+        double m = 28 - 5 / 2.0;
+        double n = 15.0 + 3.0 * 2.0 / 5.0;
+        System.out.println("a) 4 + 6 * 2 = " + a);
+        System.out.println("b) 10 / 5 + 8 = " + b);
+        System.out.println("c) 12 / 4 + 16 / 2 = " + c);
+        System.out.println("d) 17 / 2 = " + d);
+        System.out.println("e) 39 / 10 = " + e);
+        System.out.println("f) 19 % (2 + 3) = " + f);
+        System.out.println("g) 3 + 4 * 20 / 3 = " + g);
+        System.out.println("h) 36 % (6 + 2) = " + h);
+        System.out.println("i) 8 % 2 * 0 = " + i);
+        System.out.println("j) 3 - 5 % 7 = " + j);
+        System.out.println("k) 20 / (4 + 1) = " + k);
+        System.out.println("l) 18.0 / 4 = " + l);
+        System.out.println("m) 28 - 5 / 2.0 = " + m);
+        System.out.println("n) 15.0 + 3.0 * 2.0 / 5.0 = " + n);
+    }
+}
